fdcbf266a0f00c342f212b3ded81c9485ea2d726
Mohit Garg <gargmohit@live.in>
2017-04-05 05:03:55 +0530

    skidaycount component added

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import {Component} from 'react'
 import '../stylesheets/ui.scss'
 
-const SkiDayCount = React.createClass({
-	render() {
-		return (
-			<div className="ski-day-count">
-				<div className="total-days">
-					<span>5 days</span>
-				</div>
-				<div className="powder-days">
-					<span>2 days</span>
-				</div>
-				<div className="backcountry-days">
-					<span>1 hiking day</span>
-				</div>
-			</div>
-		)
-	}
-})
+const percentToDecimal = (decimal) => `${decimal * 100} %`
+
+const calcGoalProgress = (total, goal) => percentToDecimal(total / goal)
+
+const SkiDayCount = (props) => (
+    <div className="ski-day-count">
+        <div className="total-days">
+            <span>{props.total}</span>
+            <span>days</span>
+        </div>
+        <div className="powder-days">
+            <span>{props.powder}</span>
+            <span>days</span>
+        </div>
+        <div className="backcountry-days">
+            <span>{props.backcountry}</span>
+            <span>day</span>
+        </div>
+        <div>
+            <span>{calcGoalProgress(props.total, props.goal)}</span>
+        </div>
+    </div>
+)
 
 export default SkiDayCount
