7c14d375d4cc050dc0d7d3fb6713eda2a75ba085
Mohit Garg <gargmohit@live.in>
2017-04-05 05:06:48 +0530

    syntax simplifies

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,26 +1,25 @@
-import {Component} from 'react'
 import '../stylesheets/ui.scss'
 
 const percentToDecimal = (decimal) => `${decimal * 100} %`
 
 const calcGoalProgress = (total, goal) => percentToDecimal(total / goal)
 
-const SkiDayCount = (props) => (
+const SkiDayCount = ({ total, powder, backcountry, goal }) => (
     <div className="ski-day-count">
         <div className="total-days">
-            <span>{props.total}</span>
+            <span>{total}</span>
             <span>days</span>
         </div>
         <div className="powder-days">
-            <span>{props.powder}</span>
+            <span>{powder}</span>
             <span>days</span>
         </div>
         <div className="backcountry-days">
-            <span>{props.backcountry}</span>
+            <span>{backcountry}</span>
             <span>day</span>
         </div>
         <div>
-            <span>{calcGoalProgress(props.total, props.goal)}</span>
+            <span>{calcGoalProgress(total, goal)}</span>
         </div>
     </div>
 )
